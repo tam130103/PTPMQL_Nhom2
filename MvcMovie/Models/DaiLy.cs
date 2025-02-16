e09ee959f064a8e635cc29f38d38939f026905f5
tam130103 <thetam2103@gmail.com>
2025-02-16 16:36:15 +0700

    Ngày 10/02

---
+++ MvcMovie/Models/DaiLy.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Net.Http.Headers;
+
+namespace MvcMovie.Models;
+
+public class DaiLy
+{
+    public string? MaDaiLy  {get; set; }
+    public string? TenDaiLy   {get; set; }
+    public string? DiaChi   {get; set; }
+    public string? NguoiDaiDien  {get; set; }
+    public string? DienThoai  {get; set; }
+    public string? MaHTPP  {get; set; }
+}
---
+++ MvcMovie/Models/Employee.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models;
+
+public class Employee : Person
+{
+    public string? EmployeeId   {get; set; }
+
+    public int Age  {get; set; }
+
+}
